440f5111771413ecda071afbee3601577e786482
felix <1054478332@qq.com>
2017-12-13 13:19:26 +0800

    综合输出

--- test.php
+++ test.php
@@ -1,9 +1,10 @@
 <?php
 require_once('./response.php');
-$arr=array(
+/*$arr=array(
     'id'=>1,
     'name'=>'wf'
-);
+);*/
+$arr=array('dd','ss','ss');
+//echo Response::json('200','返回数据成功',$arr);
 
-echo Response::json('200','返回数据成功',$arr);
-
+echo Response::show('200','返回数据成功',$arr,'xml');
